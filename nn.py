cc5021214fdf04ef7ba4705309b918025d243e15
rheinonen <rheinonen@ucsd.edu>
2019-05-07 17:40:54 -0700

    Update nn.py

--- nn.py
+++ nn.py
@@ -17,6 +17,8 @@ def shuffle_in_unison(a, b):
     np.random.set_state(rng_state)
     np.random.shuffle(b)
 
+#import outputs of post_process.py for several simulations. kappa=constant density gradient drive, beta=curved density gradient drive
+# neg1,neg2,neg3 enforce symmetry constraints of exact equations under sign inversion
 data=np.array([]).reshape(0,5)
 label=[]
 dir_list=('kappa=0.75','kappa=1','kappa=1.25','kappa=1.5','kappa=1.75','kappa=2','kappa=2.25','kappa=2.5','kappa=2.75','kappa=3','beta=1','beta=1.5','beta=2','beta=2.5','beta=3','beta=4','beta=5','beta=6','beta=7','beta=8','beta=9','beta=10')#,'q=10','q=15','q=20','q=25','q=30')
@@ -41,32 +43,33 @@ print(data.shape)
 #train_weight=w[20001:]
 #test_weight=w[:20000]
 
+#train model several times to make sure independent of training/validation partition
 n_models=10
 models = [Sequential() for i in range(0,n_models)]
 feat=['ens','vort','n_x','vort_x','ens_x']
 for i in range(0,n_models):
     shuffle_in_unison(data,label)
+    #randomly partition data
     train_data=data[:2000000,:]
     test_data=data[2000001:,:]
     train_label=label[:2000000]
     test_label=label[2000001:]
 
     models[i].add(Dense(units=10, input_dim=5,kernel_regularizer=regularizers.l2(0.1)))
-    #models[i].add(BatchNormalization())
+ 
     models[i].add(ELU(alpha=1))
     models[i].add(BatchNormalization())
     #models[i].add(Dropout(0.5))
     models[i].add(Dense(units=10,kernel_regularizer=regularizers.l2(0.1)))
-    #models[i].add(BatchNormalization())
     models[i].add(ELU(alpha=1))
     models[i].add(BatchNormalization())
     #models[i].add(Dropout(0.5))
     models[i].add(Dense(units=10, kernel_regularizer=regularizers.l2(0.1)))
-    #models[i].add(BatchNormalization())
     models[i].add(ELU(alpha=1))
     models[i].add(BatchNormalization())
     #models[i].add(Dropout(0.5))
     models[i].add(Dense(units=1))
+    #logcosh makes the model more robust to large fluctuations
     models[i].compile(loss='logcosh', optimizer='adam')
 
     callbacks = [EarlyStopping(monitor='val_loss', patience=10),
@@ -78,18 +81,4 @@ for i in range(0,n_models):
                           verbose=1, # Print description after each epoch
                           batch_size=256, # Number of observations per batch
                           validation_data=(test_data, test_label)) # Data for evaluation
-    #models[i].save("n_"+str(i)+'_nn.h5')
-
-#enslist=(0.1, 10,20, 30, 40)
-#av=np.zeros(3001)
-#for ens in enslist:
-#    predict_data=np.transpose([np.ones(1001)*ens,np.ones(1001), [i/100. for i in range(-500,501)],np.zeros(1001),np.zeros(1001)])
-#    for i in range(0,5):
-    #model.fit(train_data,train_label,sample_weight=train_weight,eval_set=[(test_data, test_label)],sample_weight_eval_set=[test_weight],early_stopping_rounds=50)
-#        predicted_flux=models[i].predict(predict_data)
-        #plt.scatter([i/100. for i in range(0,1501)],predicted_flux)
-#        av=av+predicted_flux
-#    av=av/5.
-#    plt.plot([i/100. for i in range(-500,501)],predicted_flux)
-#plt.legend(['ens='+str(ens) for ens in enslist])
-#plt.show()
+   

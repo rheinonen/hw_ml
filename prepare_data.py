5ef2b610d05b1c788b7a1074f9814e041a5e86f7
rheinone <rheinone@ucsd.edu>
2019-05-21 19:27:39 -0700

    bug fixes, added print statements, reduced memory usage

--- prepare_data.py
+++ prepare_data.py
@@ -9,10 +9,12 @@ phi=bf.Field(name='phi',dx=0.1)
 n=bf.Field(name='n',dx=0.1)
 vort=bf.Field(name='vort',dx=0.1)
 
+print('collecting data')
 phi.collect()
 n.collect()
 vort.collect()
 
+print('computing mean fields')
 #extract relevant mean fields
 phi_zonal=phi.zonal()
 phi_fluc=phi.fluc()
@@ -20,13 +22,25 @@ n_zonal=n.zonal()
 n_fluc=n.fluc()
 vort_zonal=vort.zonal()
 vort_fluc=vort.fluc()
+
+trange=n.dims[0]
+#clear some memory
+del phi
+del n
+del vort
+
 q_fluc=n_fluc-vort_fluc
+
+del n_fluc
+
 ens_zonal=0.5*(q_fluc**2).zonal()
+del q_fluc
 vx=phi_fluc.deriv(2)
+del phi_fluc
 #ens_flux=0.5*((q_fluc**2)*vx).zonal()
 #n_flux=(n_fluc*vx).zonal()
 vort_flux=(vort_fluc*vx).zonal()
-
+del vort_fluc
 #build array corresponding to x points
 x=[i for i in range(0,516)]
 x=np.divide(x,515.)
@@ -36,13 +50,17 @@ x=np.divide(x,515.)
 #beta=
 #bg=(-0.5*beta*np.square(x)+0.5*beta)*515*0.1
 
+print('adding in background density')
 k=1
 bg=k*515*0.1*x
-bg=np.repeat(bg[np.newaxis,:,np.newaxis],n.dims[0],axis=0)
+bg=np.repeat(bg[np.newaxis,:,np.newaxis],trange,axis=0)
 n_zonal.data=np.add(n_zonal.data,bg)
 
 xpoints=16
 tmin=25
+
+print('coarse graining and packaging data')
+
 #prepare data for analysis
 
 #ens_flux_std=ens_flux.stddev(xpoints).clean(tmin)

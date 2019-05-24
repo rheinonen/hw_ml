79830c516f667d151a9e388630aee372e4e75678
rheinone <rheinone@ucsd.edu>
2019-05-23 21:14:49 -0700

    added nonlocal model

---
+++ nonlocal_nn.py
@@ -0,0 +1,60 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense,GRU,Bidrectional,TimeDistributed
+from keras import losses
+from keras.layers.advanced_activations import ELU
+from keras.layers.normalization import BatchNormalization
+from keras import regularizers
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+import sklearn as skl
+
+def shuffle_in_unison(a, b):
+    rng_state = np.random.get_state()
+    np.random.shuffle(a)
+    np.random.set_state(rng_state)
+    np.random.shuffle(b)
+
+data=np.array([]).reshape(0,512,3)
+label=np.array([]).reshape(0,512)
+dir_list=('kappa=0.75','kappa=1','kappa=1.25','kappa=1.5','kappa=1.75','kappa=2','kappa=2.25','kappa=2.5','kappa=2.75','kappa=3','beta=1','beta=1.5','beta=2','beta=2.5','beta=3','beta=4','beta=5','beta=6','beta=7','beta=8','beta=9','beta=10')#,'q=10','q=15','q=20','q=25','q=30')
+for dir in dir_list:
+    file=np.load(dir+'/data/cleaned_data_nonlocal.npz')
+    newdata=np.stack((file['n'],file['vort'],file['ens']),axis=2)
+    neg1=np.stack((np.flip(file['n'],axis=1),np.flip(file['vort'],axis=1),np.flip(file['ens'],axis=1)),axis=2) #x-> -x, y-> -y
+    neg2=np.stack(-(np.flip(file['n'],axis=1),-np.flip(file['vort'],axis=1),np.flip(file['ens'],axis=1)),axis=2) #x-> -x, phi-> -phi, n-> -n
+    neg3=np.stack(-(file['n'],-file['vort'],file['ens']),axis=2) #y-> -y, phi-> -phi, n-> -n
+    #print(newdata.shape)
+    data=np.concatenate((data,newdata,neg1,neg2,neg3),axis=0)
+    #print(data.shape)
+    newlabel=file['n_flux']
+    #print(newlabel.shape)
+    label=np.concatenate((label,-np.flip(newlabel,axis=1),np.flip(newlabel,axis=1),np.flip(newlabel,axis=1),-newlabel),axis=0)
+
+print(data.shape)
+
+model=Sequential()
+
+feat=['n','vort','ens']
+
+ntrain=140000
+
+#randomly partition data
+shuffle_in_unison(data,label)
+train_data=data[:ntrain,:]
+val_data=data[ntrain+1:,:]
+train_label=label[:ntrain]
+val_label=label[ntrain+1:]
+
+model.add(Bidirectional(GRU(512,input_dim=(512,3),return_sequences=True,dropout=0,recurrent_dropout=0)))
+model.add(TimeDistributed(Dense(512)))
+model.compile(loss='mean_squared_error', optimizer='adam')
+callbacks = [EarlyStopping(monitor='val_loss', patience=10),
+             ModelCheckpoint(filepath='n_nonlocal.h5', monitor='val_loss', save_best_only=True)]
+
+history = model.fit(train_data, # Features
+                      train_label, # Target vector
+                      epochs=10000, # Number of epochs
+                      callbacks=callbacks, # Early stopping
+                      verbose=1, # Print description after each epoch
+                      batch_size=256, # Number of observations per batch
+                      validation_data=(val_data, val_label)) # Data for evaluation
---
+++ prepare_data_nonlocal.py
@@ -0,0 +1,73 @@
+from boutdata.collect import collect
+from boututils import calculus as calc
+import math
+import numpy as np
+import bout_field as bf
+
+phi=bf.Field(name='phi',dx=0.1)
+n=bf.Field(name='n',dx=0.1)
+vort=bf.Field(name='vort',dx=0.1)
+
+print('collecting data')
+phi.collect()
+n.collect()
+vort.collect()
+
+print('computing mean fields')
+#extract relevant mean fields
+phi_zonal=phi.zonal()
+phi_fluc=phi.fluc()
+n_zonal=n.zonal()
+n_fluc=n.fluc()
+vort_zonal=vort.zonal()
+vort_fluc=vort.fluc()
+
+trange=n.dims[0]
+#clear some memory
+del phi
+del n
+del vort
+
+q_fluc=n_fluc-vort_fluc
+
+del n_fluc
+
+ens_zonal=0.5*(q_fluc**2).zonal()
+del q_fluc
+vx=phi_fluc.deriv(2)
+del phi_fluc
+ens_flux=0.5*((q_fluc**2)*vx).zonal()
+n_flux=(n_fluc*vx).zonal()
+vort_flux=(vort_fluc*vx).zonal()
+del vort_fluc
+#build array corresponding to x points
+x=[i for i in range(0,516)]
+x=np.divide(x,515.)
+
+#add in background density field
+
+#beta=
+#bg=(-0.5*beta*np.square(x)+0.5*beta)*515*0.1
+
+print('adding in background density')
+k=1
+bg=k*515*0.1*x
+bg=np.repeat(bg[np.newaxis,:,np.newaxis],trange,axis=0)
+n_zonal.data=np.add(n_zonal.data,bg)
+
+tmin=25
+n=n_zonal.clean(tmin,flatten=False)
+vort=vort_zonal.clean(tmin,flatten=False)
+ens=ens_zonal.clean(tmin,flatten=False)
+ens_flux=ens_flux.clean(tmin,flatten=False)
+n_flux=n_flux.clean(tmin,flatten=False)
+vort_flux=vort_flux.clean(tmin,flatten=False)
+
+print(n.size)
+print(vort.size)
+print(ens.size)
+print(vort_flux.size)
+print(ens_flux.size)
+print(n_flux.size)
+
+np.savez('cleaned_data_nonlocal.npz',n=n,ens=ens,vort=vort,ens_flux=ens_flux,vort_flux=vort_flux,n_flux=n_flux)
